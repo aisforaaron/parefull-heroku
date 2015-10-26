96d4c005ea9982e9bad09e73651388ccf0bd7e90
Aaron Deutsch <aarondllc@gmail.com>
2015-10-26 14:58:46 -0500

    heroku deploy testing

--- config/defaults.js
+++ config/defaults.js
@@ -1,7 +1,7 @@
 module.exports = {
     env : 'default',
     address : '127.0.0.1',
-    port : process.env.PORT || 4000,
+    port : 4000,
     secret : "Your secret here",
     db : {
         url : 'mongodb://localhost/local'
