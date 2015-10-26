1d7b5fae26ce1748f83b1b65b8ba7827b61e0c6e
Aaron Deutsch <aarondllc@gmail.com>
2015-10-26 14:22:41 -0500

    Updated privacy text with google image info.

--- docroot/assets/js/site.js
+++ docroot/assets/js/site.js
@@ -3,6 +3,11 @@ var aboutText = `
 Thanks for stopping by to checkout my Node.js app.
 This is just a test project to work with the Expressjs/Node.js framework.
 
+All images for bits are found from public Google image searches and are not the property of this site.
+
+Otherwise Copyright &copy; 2015 Aaron D LLC 
+
+
 #### Application Info
 
 * [Nginx](https://www.nginx.com) - static web server and RESTful API proxy
@@ -51,6 +56,7 @@ If you feel that we are not abiding by this privacy policy, you should contact u
 ### Content on the Services
 
 * All Content, whether publicly posted or privately transmitted, is the sole responsibility of the person who originated such Content. We may not monitor or control the Content posted via the Services and, we cannot take responsibility for such Content. Any use or reliance on any Content or materials posted via the Services or obtained by you through the Services is at your own risk.
+* All images for bits are found from public Google image searches and are not the property of this site.
 * We do not endorse, support, represent or guarantee the completeness, truthfulness, accuracy, or reliability of any Content or communications posted via the Services or endorse any opinions expressed via the Services. You understand that by using the Services, you may be exposed to Content that might be offensive, harmful, inaccurate or otherwise inappropriate, or in some cases, postings that have been mislabeled or are otherwise deceptive. Under no circumstances will Aaron D LLC be liable in any way for any Content, including, but not limited to, any errors or omissions in any Content, or any loss or damage of any kind incurred as a result of the use of any Content posted, emailed, transmitted or otherwise made available via the Services or broadcast elsewhere.
 
 
@@ -63,4 +69,6 @@ If you feel that we are not abiding by this privacy policy, you should contact u
 * Any material downloaded or otherwise obtained through your use of our services is done at your own discretion and risk, and you will be solely responsible for any damage to your computer system or loss of data that results from the download of any such material. You agree that we have no responsibility or liability for the deletion of, or the failure to store or to transmit, any content or communication maintained by the service. We retain the right to create limits on use and storage at our sole discretion at any time with or without notice.
 * Your use of our services is at your sole risk. We provide these services on an "as is" and "as available" basis, and we expressly disclaim all express or implied warranties of all kinds, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, and non-infringement. We make no warranty that our services will meet your requirements, be safe, secure, uninterrupted, timely, accurate, or error-free, or that your information will be secure.
 * Limitation on liability. In no event will Aaron D LLC, its affiliates or their licensors, service providers, employees, agents, officers, or directors be liable for damages of any kind, under any legal theory, arising out of or in connection with your use, or inability to use, the Website, any websites linked to it, any content on the Website or such other websites or any services or items obtained through the Website or such other websites, including any direct, indirect, special, incidental, consequential or punitive damages, including but not limited to, personal injury, pain and suffering, emotional distress, loss of revenue, loss of profits, loss of business or anticipated savings, loss of use, loss of goodwill, loss of data, and whether caused by tort (including negligence), breach of contract or otherwise, even if foreseeable. The foregoing does not affect any liability which cannot be excluded or limited under applicable law. 
+
+Copyright &copy; 2015 Aaron D LLC 
 `;
