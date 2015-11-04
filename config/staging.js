66a01d0f61ec99cd974eb63fa81fbb441628b933
Aaron Deutsch <aarondllc@gmail.com>
2015-11-04 14:13:09 -0600

    Adding a staging config file.

---
+++ config/staging.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env : 'staging',
+    address : '127.0.0.1',
+    port : 4000,
+    secret : "Your secret here",
+    bitFilePath:'assets/images/bits/',
+    db : {
+      url : 'mongodb://parestageuser:54UQYsjXP8vL@ds049094.mongolab.com:49094/parefull-stage'
+    }
+};
