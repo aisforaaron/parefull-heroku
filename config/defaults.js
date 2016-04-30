a1004dd052612c4043d00208124101e1cea64f4d
Aaron Deutsch <aarondllc@gmail.com>
2016-04-30 16:25:50 -0400

    Updated config/default.js to match other configs.

--- config/defaults.js
+++ config/defaults.js
@@ -1,5 +1,6 @@
 module.exports = {
     env : 'default',
+    host : '',
     address : '127.0.0.1',
     port : 4000,
     secret : "Your secret here",
@@ -8,7 +9,14 @@ module.exports = {
     db : {
         url : 'mongodb://localhost/local'
     },
-    redis : {
-        url: 'redis://127.0.0.1:6379?prefix=q'
-    }
+    // redis : {
+    //     url: 'redis://127.0.0.1:6379?prefix=q'
+    // },
+    workerInterval: process.env.APP_WORKER_INTERVAL || 900000, // 15min default
+    googleSearchAPI : {
+        url: process.env.GOOGLE_API_URL,
+        key: process.env.GOOGLE_API_KEY,
+        cx:  process.env.GOOGLE_API_CX
+    },
+    apiSecret: process.env.APP_API_SECRET,
 };
