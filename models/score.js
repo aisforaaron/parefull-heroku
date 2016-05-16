d540dfa8513da6f79f6c7a02025e8a9c5e8a9592
Aaron Deutsch <aarondllc@gmail.com>
2016-05-16 17:02:04 -0400

    Fixed bit ref in score model.

--- models/score.js
+++ models/score.js
@@ -7,7 +7,7 @@ var Schema      = mongoose.Schema;
 var ScoreSchema = new Schema({
     score: {type: Number, min: 1, max: 10},
     created: {type: Date, default: Date.now},
-    _bitId: {type: Schema.Types.ObjectId},      // to reference Bit._id
+    _bitId: {type: Schema.Types.ObjectId, ref: 'Bit'},
     ip: {type: String}
 });
 module.exports  = mongoose.model('Score', ScoreSchema);
